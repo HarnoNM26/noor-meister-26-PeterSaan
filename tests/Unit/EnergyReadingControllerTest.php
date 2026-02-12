0709c1e444b758765242ca8c79cfdd7787c6497e
Peter Saan <saanpeter@gmail.com>
2026-02-12 13:40:57 +0200

    Init controller tests

---
+++ tests/Unit/EnergyReadingControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class EnergyReadingControllerTest extends TestCase
+{
+    public function test_invalid_dates_return_an_error() {
+        $res = $this->post('/api/sync/prices', []);
+    }
+}
